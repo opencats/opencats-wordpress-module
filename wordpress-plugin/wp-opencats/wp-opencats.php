4b5ae42d8ba3c3cae0fccb503cc36db72106d355
Robin Layfield <robin@ultrasimplified.com>
2014-10-16 08:03:31 +0100

    Basic plugin structure

--- wordpress-plugin/wp-opencats/wp-opencats.php
+++ wordpress-plugin/wp-opencats/wp-opencats.php
@@ -13,5 +13,25 @@ define( 'OCATS_URL',     plugin_dir_url( __FILE__ )  );
 define( 'OCATS_PATH',    plugin_dir_path( __FILE__ ) );
 define( 'OCATS_VERSION', '0.1'                       );
 
-function ocats_init() {}
-add_action( 'plugins_loaded', 'ocats_init' );
+function opencats_init() {
+	add_action( 'wp_enqueue_scripts', 'opencats_enqueue_scripts' );
+}
+add_action( 'plugins_loaded', 'opencats_init' );
+
+function ocats_enqueue_scripts() {
+    wp_enqueue_style( 'opencats', OCATS_URL . '/assets/css/opencats.css', array(), OCATS_VERSION );
+}
+
+function ocats_admin_settings(){
+	add_options_page( 'OpenCATS', 'OpenCATS', 'manage_options', 'opencats', 'opencats_plugin_options' );
+}
+add_action( 'admin_menu', 'ocats_admin_settings' );
+
+function opencats_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
